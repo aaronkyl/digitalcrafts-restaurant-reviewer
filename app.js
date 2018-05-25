b213bc75f91fd43dfd6ca92b5508fc41fbe04669
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-05-25 04:28:30 +0000

    working

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
+const pgp = require('pg-promise')({});
+const db = pgp({database: 'restaurant_v2', user: 'postgres'});
 
 const app = express();
 
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  noCache: true
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
+
+app.get("/", function(request, response) {
+    response.render('index.html', {});
+});
+
+app.get("/search", function(request, response) {
+    let searchTerm = request.query.searchTerm;
+    db.any('SELECT * FROM restaurant WHERE name ILIKE $1', ['%' + searchTerm + '%'])
+    .then(function(results) {
+        response.render('search_results.html', {restaurants: results});
+    })
+    .catch(function(error) {
+        console.error("DATABASE ERROR: ", error);
+        response.redirect('/');
+    });
+});
 
 app.listen(8080, function() {
     console.log("app started on port 8080");
